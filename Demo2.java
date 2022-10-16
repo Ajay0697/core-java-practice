cfd4f2c4730caca0c32fc9f63ae5b3d150687dae
Ajay0697 <115772670+Ajay0697@users.noreply.github.com>
2022-10-16 22:30:03 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,23 @@
+/*
+program:To understand various data type in java
+@author:Ajay kumar
+@Date:14 oct 2022
+*/
+
+//declaring a class
+class Demo2
+{
+//main method
+public static void main(String[]args)
+   {
+      //declaring local variables
+	  int number1=67;
+	  int number2=35;
+	  // calculating the sum
+	  int sum = number1 + number2;
+	  System.out.println("the sum is :"+sum);//print the value of sum
+	  
+     }
+    //end of main
+}
+//end of class Demo2
