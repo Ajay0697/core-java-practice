eac78d3d5c2ea6b455ec6fb34b1fa17697e69754
Ajay0697 <115772670+Ajay0697@users.noreply.github.com>
2022-10-16 22:31:09 +0530

    Add files via upload

---
+++ PrintName.java
@@ -0,0 +1,10 @@
+class PrintName
+{
+public static void main(String[]args)
+{
+System.out.println("name:"+args[0]);
+System.out.println("age:"+args[1]);
+System.out.println("course:"+args[2]);
+System.out.println("grade:"+args[3]);
+}
+}
