e413584766060f668df7a896d1a071fbbdb5265f
Ajay0697 <115772670+Ajay0697@users.noreply.github.com>
2022-10-16 21:58:00 +0530

    Add files via upload

---
+++ AdditionDemo.java
@@ -0,0 +1,23 @@
+
+
+// declaring a class
+class AdditionDemo
+{
+//main method
+public static void main(String[]args)
+{
+//declaring local variables
+int number1= Integer.parseInt(args[0]);
+int number2= Integer.parseInt(args[1]);
+
+//calculating the sum
+int Sum = number1+number2;
+System.out.println("the sum is :" +Sum);  //printing the value of sum
+}
+
+//end of main 
+
+}
+//end of class AdditionDemo
+
+
---
+++ DataTypeDemo.java
@@ -0,0 +1,19 @@
+class DataTypeDemo
+{
+     public static void main(String []args)
+	 {
+              String name="AJAY KUMAR";
+			  char gender='M';
+			  int age=30;
+			  float marks=89.7f;
+			  boolean pass=true;
+			  
+			  
+			  System.out.println("Name:"+name);
+			  System.out.println("Gender:"+gender);
+			  System.out.println("Age:"+age);
+			  System.out.println("Marks:"+marks);
+			  System.out.println("Name:"+name);
+			  }
+			  
+}
